76b29ca2556fcf7367ae23dc33e08a3c8cd5fa6c
Pascal Schröder <paschroeder@uni-osnabrueck.de>
2020-02-28 15:44:52 +0100

    create file for past recordings page

---
+++ lib/past_recordings.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PastRecordingsPage extends StatefulWidget {
+  @override
+  _PastRecordingsPageState createState() => _PastRecordingsPageState();
+}
+
+class _PastRecordingsPageState extends State<PastRecordingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Past Recordings'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go back'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );  }
+}
